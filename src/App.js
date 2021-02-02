02bad227e65df73e5e096a6e0039f94d9abdef2d
sanyath007 <sanyath007@gmail.com>
2021-02-02 17:55:24 +0700

    Create lib to fetch products from commerce js and use its in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { commerce } from './lib/commerce';
 import { Navbar, Products } from './components';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    console.log(products);
+
     return (
         <div>
             <Navbar />
-            <Products />
+            <Products products={products} />
         </div>
     )
 }
