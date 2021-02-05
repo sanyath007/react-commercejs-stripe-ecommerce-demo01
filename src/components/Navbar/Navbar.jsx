53e3ad02bb877186d160baeac4366647928793cb
sanyath007 <sanyath007@gmail.com>
2021-02-06 05:43:29 +0700

    Add Link component to Navbar to navigate app

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,26 +3,30 @@ import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '
 import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = (props) => {
     const classes = useStyles();
-    
+    const location = useLocation();
+
     return (
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
                         Commerce.js
                     </Typography>
                     <div className={classes.grow} />
-                    <div>
-                        <IconButton aria-label="Show cart items" color="inherit">
-                            <Badge badgeContent={props.totalItems} color="secondary">
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                    </div>
+                    { location.pathname === '/' && (
+                        <div>
+                            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                                <Badge badgeContent={props.totalItems} color="secondary">
+                                    <ShoppingCart />
+                                </Badge>
+                            </IconButton>
+                        </div>
+                    )}
                 </Toolbar>
             </AppBar>
         </>
